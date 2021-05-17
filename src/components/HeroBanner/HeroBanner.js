398f68a424603b45f99aa5ba16e4cbb57dc53f9c
TomLemelle <tom.lemelle@hotmail.fr>
2021-05-17 12:02:06 +0200

    heroBanner desktop

--- src/components/HeroBanner/HeroBanner.js
+++ src/components/HeroBanner/HeroBanner.js
@@ -4,13 +4,13 @@ const HeroBanner = () => {
     return (
         <section className="hero-banner">
             <ul className="wrapper-hero-banner">
-                <li>
+                <li className={"first-li"}>
                     <h1>Project Title</h1>
                 </li>
-                <li>
+                <li className={"second-li"}>
                     <img src="./assets/img/hero-banner.jpg" alt="image hero banner"/>
                 </li>
-                <li>
+                <li className={"last-li"}>
                     <p>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit,
                         vulputate eu pharetra nec, mattis ac.
